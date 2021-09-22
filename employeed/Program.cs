6537067d9179f310982bf1f3f889dd6c1ee9f3c6
varnikak00 <varnikak7070@gmail.com>
2021-09-22 18:10:06 +0530

    chnge git

---
+++ employeed/Program.cs
@@ -0,0 +1,30 @@
+﻿int EmpHrs = 0;
+int EmpWage = 0;
+int TotalEmployeeWage = 0;
+
+for (int day = 0; day < NUmOfWorkingDays; day++)
+{
+    Random random = new Random();
+
+    int EmpCheck = random.Next(3);
+    switch (EmpCheck)
+    {
+        case IsPartTime:
+            EmpHrs = 4;
+            break;
+
+        case IsFullTime:
+            EmpHrs = 8;
+            break;
+
+        default:
+            EmpHrs = 8;
+            break;
+    }
+    EmpWage = EmpHrs * EmpRatePerHour;
+    TotalEmployeeWage += EmpWage;
+    Console.WriteLine("Emp Wage :" + EmpWage);
+}
+Console.WriteLine(" Total Emp Wage :" + TotalEmployeeWage);
+        }
+    }
